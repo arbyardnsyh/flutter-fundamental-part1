8f739c2cad6431a92ef09e1d1b57450cff885092
arby ardiansyah <arbiardiansyah@gmail.com>
2024-09-24 00:25:34 +0700

    Update all project files

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,57 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_arby/widget_baru/images_widget.dart';
 import 'package:flutter_arby/widget_baru/text_widget.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Contoh Date Picker',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'arby.ardnsyh_ project'),
-      color: const Color.fromARGB(255, 48, 64, 114),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  DateTime selectedDate = DateTime.now();
 
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
   }
 
   @override
@@ -48,13 +61,23 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      backgroundColor: const Color.fromARGB(255, 3, 34, 49), // Menambahkan warna latar belakang
+      backgroundColor: const Color.fromARGB(255, 3, 34, 49),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            TextWidget(),
-            ImageWidget(),
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text('$_counter', style: Theme.of(context).textTheme.displayMedium), // Atau titleLarge
+            const TextWidget(),
+            const ImageWidget(),
+            const MyLayout(),
+            const SizedBox(height: 20),
+            Text("${selectedDate.toLocal()}".split(' ')[0]), // Menampilkan tanggal yang dipilih
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () => _selectDate(context),
+              child: const Text('Pilih Tanggal'),
+            ),
           ],
         ),
       ),
@@ -65,4 +88,45 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+class MyLayout extends StatelessWidget {
+  const MyLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        child: const Text('Profil'),
+        onPressed: () {
+          showAlertDialog(context);
+        },
+      ),
+    );
+  }
+}
+
+void showAlertDialog(BuildContext context) {
+  Widget okButton = TextButton(
+    child: const Text("OK"),
+    onPressed: () {
+      Navigator.pop(context);
+    },
+  );
+
+  AlertDialog alert = AlertDialog(
+    title: const Text("My Profile"),
+    content: const Text("NAMA:MOH. ARBI ARDRIANSYAH. NIM:362358302150. Kelas:2B TRPL "),
+    actions: [
+      okButton,
+    ],
+  );
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
